da5fb1bd0525a51697b8c1fe55ea67dca9cdae82
User Name <user@example.com>
2025-12-05 03:02:51 +0300

    Fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,3 @@
-import { Toaster } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
@@ -26,10 +24,7 @@ function App() {
       <ThemeProvider
         defaultTheme="light"
       >
-        <TooltipProvider>
-          <Toaster />
           <Router />
-        </TooltipProvider>
       </ThemeProvider>
     </ErrorBoundary>
   );
